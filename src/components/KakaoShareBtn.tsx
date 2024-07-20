8476d04ee367501b5336a3e1ecc41c88ce487fd4
MMyeong <ilikerunning@naver.com>
2024-07-20 16:20:50 +0900

    chore: kakao share 수정

--- src/components/KakaoShareBtn.tsx
+++ src/components/KakaoShareBtn.tsx
@@ -10,12 +10,13 @@ declare global {
 interface Kakao {
   init: (apiKey: string) => void;
   isInitialized: () => boolean;
-  Link: {
-    sendDefault: (linkObject: KakaoLinkObject) => void;
+  Share: {
+    createDefaultButton: (options: KakaoLinkObject) => void;
   };
 }
 
 interface KakaoLinkObject {
+  container: string;
   objectType: string;
   content: {
     title: string;
@@ -25,6 +26,11 @@ interface KakaoLinkObject {
       mobileWebUrl: string;
       webUrl: string;
     };
+  };
+  social: {
+    likeCount: number;
+    commentCount: number;
+    sharedCount: number;
   };
   buttons: Array<{
     title: string;
@@ -44,14 +50,11 @@ export const KakaoShareButton: React.FC<KakaoShareButtonProps> = ({
 }) => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js";
+    script.src = "https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js";
+    script.integrity =
+      "sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4";
+    script.crossOrigin = "anonymous";
     script.async = true;
-
-    script.onload = () => {
-      if (!window.Kakao.isInitialized()) {
-        window.Kakao.init(`${import.meta.env.VITE_KAKAO_APP_KEY}`);
-      }
-    };
     document.body.appendChild(script);
     return () => {
       document.body.removeChild(script);
@@ -59,33 +62,52 @@ export const KakaoShareButton: React.FC<KakaoShareButtonProps> = ({
   }, []);
 
   const shareToKakao = (shareUrl: string) => {
-    if (window.Kakao) {
-      window.Kakao.Link.sendDefault({
-        objectType: "feed",
-        content: {
-          title: "카카오톡 공유하기",
-          description: "이것은 카카오톡 공유 예제입니다.",
-          imageUrl: "IMAGE_URL_HERE",
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(`${import.meta.env.VITE_KAKAO_API_KEY}`);
+    }
+    window.Kakao.Share.createDefaultButton({
+      container: "#kakaotalk-sharing-btn",
+      objectType: "feed",
+      content: {
+        title: "PrayU",
+        description: "PrayU와 함께 기도해요!",
+        imageUrl: "이미지주소",
+        link: {
+          mobileWebUrl: shareUrl,
+          webUrl: shareUrl,
+        },
+      },
+      social: {
+        likeCount: 286,
+        commentCount: 45,
+        sharedCount: 845,
+      },
+      buttons: [
+        {
+          title: "오늘의 기도",
           link: {
-            mobileWebUrl: shareUrl,
-            webUrl: shareUrl,
+            mobileWebUrl: `${import.meta.env.VITE_BASE_URL}`,
+            webUrl: `${import.meta.env.VITE_BASE_URL}`,
           },
         },
-        buttons: [
-          {
-            title: "웹으로 보기",
-            link: {
-              mobileWebUrl: shareUrl,
-              webUrl: shareUrl,
-            },
+        {
+          title: "내 기도제목",
+          link: {
+            mobileWebUrl: `${import.meta.env.VITE_BASE_URL}`,
+            webUrl: `${import.meta.env.VITE_BASE_URL}`,
           },
-        ],
-      });
-    }
+        },
+      ],
+    });
   };
 
   return (
-    <Button onClick={() => shareToKakao(webUrl)} variant="ghost" size="icon">
+    <Button
+      id="kakaotalk-sharing-btn"
+      onClick={() => shareToKakao(webUrl)}
+      variant="ghost"
+      size="icon"
+    >
       <img src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png" />
     </Button>
   );
