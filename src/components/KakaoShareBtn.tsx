ce8498cae29d57eb025cd3172cfef453d643c2b0
김민수 <minsukim@gimminsuui-MacBookAir.local>
2024-07-22 14:01:14 +0900

    wrong typing found && click at once

--- src/components/KakaoShareBtn.tsx
+++ src/components/KakaoShareBtn.tsx
@@ -55,59 +55,56 @@ export const KakaoShareButton: React.FC<KakaoShareButtonProps> = ({
       "sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4";
     script.crossOrigin = "anonymous";
     script.async = true;
+
+    script.onload = () => {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(`${import.meta.env.VITE_KAKAO_JS_KEY}`);
+      }
+      window.Kakao.Share.createDefaultButton({
+        container: "#kakaotalk-sharing-btn",
+        objectType: "feed",
+        content: {
+          title: "PrayU",
+          description: "PrayU와 함께 기도해요!",
+          imageUrl: "이미지주소",
+          link: {
+            mobileWebUrl: webUrl,
+            webUrl: webUrl,
+          },
+        },
+        social: {
+          likeCount: 286,
+          commentCount: 45,
+          sharedCount: 845,
+        },
+        buttons: [
+          {
+            title: "오늘의 기도",
+            link: {
+              mobileWebUrl: `${import.meta.env.VITE_BASE_URL}`,
+              webUrl: `${import.meta.env.VITE_BASE_URL}`,
+            },
+          },
+          {
+            title: "내 기도제목",
+            link: {
+              mobileWebUrl: `${import.meta.env.VITE_BASE_URL}`,
+              webUrl: `${import.meta.env.VITE_BASE_URL}`,
+            },
+          },
+        ],
+      });
+    };
+
     document.body.appendChild(script);
+
     return () => {
       document.body.removeChild(script);
     };
   }, []);
 
-  const shareToKakao = (shareUrl: string) => {
-    if (!window.Kakao.isInitialized()) {
-      window.Kakao.init(`${import.meta.env.VITE_KAKAO_API_KEY}`);
-    }
-    window.Kakao.Share.createDefaultButton({
-      container: "#kakaotalk-sharing-btn",
-      objectType: "feed",
-      content: {
-        title: "PrayU",
-        description: "PrayU와 함께 기도해요!",
-        imageUrl: "이미지주소",
-        link: {
-          mobileWebUrl: shareUrl,
-          webUrl: shareUrl,
-        },
-      },
-      social: {
-        likeCount: 286,
-        commentCount: 45,
-        sharedCount: 845,
-      },
-      buttons: [
-        {
-          title: "오늘의 기도",
-          link: {
-            mobileWebUrl: `${import.meta.env.VITE_BASE_URL}`,
-            webUrl: `${import.meta.env.VITE_BASE_URL}`,
-          },
-        },
-        {
-          title: "내 기도제목",
-          link: {
-            mobileWebUrl: `${import.meta.env.VITE_BASE_URL}`,
-            webUrl: `${import.meta.env.VITE_BASE_URL}`,
-          },
-        },
-      ],
-    });
-  };
-
   return (
-    <Button
-      id="kakaotalk-sharing-btn"
-      onClick={() => shareToKakao(webUrl)}
-      variant="ghost"
-      size="icon"
-    >
+    <Button id="kakaotalk-sharing-btn" variant="ghost" size="icon">
       <img src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png" />
     </Button>
   );
