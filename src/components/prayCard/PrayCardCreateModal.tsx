913866d502c4ad70d99bc36aeb0ce7cbebad2951
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-07-25 02:09:42 +0900

    fix: TextArea 줌인 버그 수정 (#30)
    * chore: test commit
    
    * chore: test commit
    
    * chore: test commit
    
    * chore: 테스트 상태 복구
    
    * fix: merge error

--- src/components/prayCard/PrayCardCreateModal.tsx
+++ src/components/prayCard/PrayCardCreateModal.tsx
@@ -34,22 +34,23 @@ const PrayCardCreateModal: React.FC<PrayCardCreateModalProps> = ({
   };
 
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen mt-4 gap-4">
+    <div className="flex flex-col items-center justify-start min-h-screen gap-4">
       <div className="w-full flex flex-col">
         <div className="text-lg font-bold">기도제목 작성 </div>
         <div className="text-sm text-gray-500">
           기도제목을 작성하면 그룹에 참여할 수 있어요
         </div>
       </div>
-      <div className="flex items-center justify-center w-full">
-        <Textarea
-          className="h-48"
-          placeholder="여기에 기도제목을 작성해주세요..."
-          value={inputPrayCardContent}
-          onChange={(e) => setPrayCardContent(e.target.value)}
-        />
-      </div>
-      <div className="mt-4 flex flex-col items-center justify-center text-center">
+      <Textarea
+        className="h-48 placeholder-gray-50"
+        style={{
+          fontSize: "16px",
+        }}
+        placeholder="기도제목을 작성해주세요"
+        value={inputPrayCardContent}
+        onChange={(e) => setPrayCardContent(e.target.value)}
+      />
+      <div className="w-full flex flex-col items-center justify-center text-center">
         <Button
           className="w-full bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => handleCreatePrayCard(currentUserId, groupId)}
