23226d6cd38a985193dc18b9151d3a9d70017000
Junbeom Park <ephesi4ns623@gmail.com>
2024-07-24 16:52:06 +0900

    chore: tailwind-foucs 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,7 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        outline: ["focus"],
       },
       borderRadius: {
         lg: "var(--radius)",
