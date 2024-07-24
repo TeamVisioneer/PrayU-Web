4798062021d71c03faf2a592434cf9e9eddd5abd
Junbeom Park <ephesi4ns623@gmail.com>
2024-07-24 17:20:03 +0900

    fix: DrawerTrigger focus outline-none

--- src/components/member/Member.tsx
+++ src/components/member/Member.tsx
@@ -58,7 +58,7 @@ const Member: React.FC<MemberProps> = ({
 
   return (
     <Drawer>
-      <DrawerTrigger>{memberUI}</DrawerTrigger>
+      <DrawerTrigger className="focus:outline-none">{memberUI}</DrawerTrigger>
       <DrawerContent className="max-w-[480px] mx-auto w-full h-[90%] px-10 pb-20">
         <DrawerHeader>
           <DrawerTitle></DrawerTitle>
