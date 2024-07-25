0c6f83d1fda30db5403c3f3e1edb35018752abdc
kim minsu <67274959+yaminsu5674@users.noreply.github.com>
2024-07-25 11:48:27 +0900

    feat: 참여 가능 그룹 수 제한(그룹 생성 제한 & 그룹 가입 제한) (#34)
    * feat: group limit related variable add
    
    * feat: if who want to make group, toast suspend that
    
    * feat: if who want to enter group, limit entering
    
    * fix: maxPossibleGrupoCount 10 -> 30 update
    
    * feedback: env, groupList.length
    
    * feedback: groupMenuBtn edit
    
    * feedback: review edit
    
    ---------
    
    Co-authored-by: 김민수 <minsukim@gimminsuui-MacBookAir.local>

--- src/components/GroupMenuBtn.tsx
+++ src/components/GroupMenuBtn.tsx
@@ -7,8 +7,10 @@ import {
   SheetTrigger,
   SheetClose,
 } from "@/components/ui/sheet";
+import { useToast } from "./ui/use-toast";
 import menuIcon from "@/assets/menuIcon.png";
 import { Group } from "supabase/types/tables";
+import { useNavigate } from "react-router-dom";
 
 interface GroupManuBtnProps {
   userGroupList: Group[];
@@ -19,44 +21,55 @@ const GroupManuBtn: React.FC<GroupManuBtnProps> = ({
   userGroupList,
   targetGroup,
 }) => {
+  const navigate = useNavigate();
+  const maxGroupCount = Number(import.meta.env.VITE_MAX_GROUP_COUNT);
+  const { toast } = useToast();
+
+  const handleClick = () => () => {
+    if (userGroupList.length < maxGroupCount) {
+      navigate("/group/new");
+    } else {
+      toast({
+        description: `최대 ${maxGroupCount}개의 그룹만 참여할 수 있어요`,
+      });
+    }
+  };
+
   return (
     <Sheet>
       <SheetTrigger className="flex flex-col items-end focus:outline-none">
-        <img src={menuIcon} alt="asdf" className="w-8 h-8" />
+        <img src={menuIcon} className="w-8 h-8" />
       </SheetTrigger>
       <SheetContent className="max-w-[288px] mx-auto w-[60%] px-5 py-16 flex flex-col items-end">
         <SheetHeader>
           <SheetTitle className="text-end">PrayU 그룹</SheetTitle>
-          <SheetClose className="focus:outline-none">
-            <div className="flex flex-col gap-4 items-end text-gray-500">
-              {userGroupList.map(
-                (group) =>
-                  group && (
-                    <a
-                      key={group.id}
-                      href={`${import.meta.env.VITE_BASE_URL}/group/${
-                        group.id
-                      }`}
-                      className={`${
-                        group.id === targetGroup?.id
-                          ? "text-black font-bold underline"
-                          : ""
-                      }`}
-                    >
-                      {group.name}
-                    </a>
-                  )
-              )}
-              <a href={`${import.meta.env.VITE_BASE_URL}/group/new`}>
-                + 그룹 만들기
-              </a>
-              <a href={`${import.meta.env.VITE_PRAY_KAKAO_CHANNEL_CHAT_URL}`}>
-                문의하기
-              </a>
-            </div>
-          </SheetClose>
           <SheetDescription></SheetDescription>
         </SheetHeader>
+        <div className="flex flex-col gap-4 items-end text-gray-500">
+          {userGroupList.map(
+            (group) =>
+              group && (
+                <a
+                  key={group.id}
+                  href={`/group/${group.id}`}
+                  className={`${
+                    group.id === targetGroup?.id
+                      ? "text-black font-bold underline"
+                      : ""
+                  }`}
+                >
+                  {group.name}
+                </a>
+              )
+          )}
+          <a className="cursor-pointer" onClick={handleClick()}>
+            + 그룹 만들기
+          </a>
+          <a href={`${import.meta.env.VITE_PRAY_KAKAO_CHANNEL_CHAT_URL}`}>
+            문의하기
+          </a>
+        </div>
+        <SheetClose className="focus:outline-none">Close</SheetClose>
       </SheetContent>
     </Sheet>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./App.css";
+import { Toaster } from "@/components/ui/toaster";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster />
     <App />
   </React.StrictMode>
 );
