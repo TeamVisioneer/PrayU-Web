e14c068dce14488d72588c33f29230aa3bf4d7d0
Junbeom Park <ephesi4ns623@gmail.com>
2024-07-25 17:20:58 +0900

    feat: 오래된 기도제목 분기 처리

---
+++ src/components/prayCard/PrayCardUIToOther.tsx
@@ -0,0 +1,45 @@
+import useBaseStore from "@/stores/baseStore";
+import PrayCardCalendar from "./WeeklyCalendar";
+import { PrayCardWithProfiles } from "supabase/types/tables";
+import ReactionBtn from "./ReactionBtn";
+import { getISOOnlyDate, getISOTodayDate } from "@/lib/utils";
+import { getDateDistance } from "@toss/date";
+import { KakaoShareButton } from "../KakaoShareBtn";
+
+interface PrayCardProps {
+  prayCard: PrayCardWithProfiles | null;
+}
+
+const PrayCardUIToOther: React.FC<PrayCardProps> = ({ prayCard }) => {
+  const prayDataHash = useBaseStore((state) => state.prayDataHash);
+  const currentUserId = useBaseStore((state) => state.user?.id);
+  const todayDate = getISOTodayDate();
+  const dateDistance = getDateDistance(
+    new Date(getISOOnlyDate(prayCard!.created_at)),
+    new Date(todayDate)
+  );
+
+  if (dateDistance.days >= 7) {
+    return (
+      <div className="flex flex-col items-center justify-center p-4 gap-4">
+        <p>기도제목이 업데이트 되지 않았어요</p>
+        <KakaoShareButton
+          groupPageUrl={window.location.href}
+          message="기도제목 요청하기"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-6">
+      <PrayCardCalendar
+        prayCard={prayCard}
+        prayData={prayDataHash[prayCard?.id || ""] || []}
+      />
+      <ReactionBtn currentUserId={currentUserId!} prayCard={prayCard} />
+    </div>
+  );
+};
+
+export default PrayCardUIToOther;
