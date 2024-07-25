50a26be832967d9fd5230352c6dbb348b60f9952
김민수 <minsukim@gimminsuui-MacBookAir.local>
2024-07-26 01:20:22 +0900

    image test

--- src/components/prayEncourage/PrayEncourageCard.tsx
+++ src/components/prayEncourage/PrayEncourageCard.tsx
@@ -7,6 +7,14 @@ export const PrayEncourageCard = () => {
         <h1>그룹원들을 초대하여 함께</h1>
         <h1 className="mb-5">기도제목을 공유하고 기도해 보아요</h1>
       </div>
+
+      <img
+        src={"/dist/PrayCard.png"}
+        sizes="
+            (max-width: 640px) 100vw,"
+        className="rounded-md object-cover"
+      />
+
       <PrayEncourageBtn />
     </div>
   );
