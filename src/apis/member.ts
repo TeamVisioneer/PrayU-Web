5ee94fa607a1741520b8c69e84aa993fa92c71ce
MMyeong <ilikerunning@naver.com>
2024-07-26 13:12:45 +0900

    chore: member update 순으로 정렬

--- src/apis/member.ts
+++ src/apis/member.ts
@@ -10,28 +10,13 @@ export const fetchMemberListByGroupId = async (
     .from("member")
     .select(`*, profiles (id, full_name, avatar_url)`)
     .eq("group_id", groupId)
-    .is("deleted_at", null);
+    .is("deleted_at", null)
+    .order("updated_at", { ascending: false });
   if (error) {
     console.error("error", error);
     return null;
   }
   return data as MemberWithProfiles[];
-};
-
-export const createMember = async (
-  groupId: string | undefined,
-  userId: string | undefined
-): Promise<Member | null> => {
-  if (!groupId || !userId) return null;
-  const { error, data } = await supabase
-    .from("member")
-    .insert([{ group_id: groupId, user_id: userId }])
-    .select();
-  if (error) {
-    console.error("error", error);
-    return null;
-  }
-  return data ? data[0] : null;
 };
 
 export const getMember = async (
