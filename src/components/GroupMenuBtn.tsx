79f3217dc618884806c1eb309a8f189bd67a19f5
MMyeong <ilikerunning@naver.com>
2024-07-29 17:25:51 +0900

    chore: 등호 제거

--- src/components/GroupMenuBtn.tsx
+++ src/components/GroupMenuBtn.tsx
@@ -28,7 +28,7 @@ const GroupManuBtn: React.FC<GroupManuBtnProps> = ({
   const signOut = useBaseStore((state) => state.signOut);
 
   const handleClick = () => () => {
-    if (userGroupList.length <= maxGroupCount) {
+    if (userGroupList.length < maxGroupCount) {
       navigate("/group/new");
     } else {
       toast({
