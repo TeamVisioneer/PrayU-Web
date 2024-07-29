97227e10dba7097c08cc90461cb09004ded5ce27
MMyeong <ilikerunning@naver.com>
2024-07-30 01:49:05 +0900

    chore: 메인페이지 패딩 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -26,7 +26,7 @@ const MainPage: React.FC = () => {
   const redirectUrl = `${baseUrl}${from}`;
 
   return (
-    <div className="flex flex-col gap-8 pt-20 text-center">
+    <div className="flex flex-col gap-8 pt-10 text-center">
       <div className="text-lg font-bold">우리만의 기도제목 기록공간 PrayU</div>
       <Carousel>
         <CarouselContent>
