53b403f9c490e84c934e2f53dd4fb55b8ab598ee
MMyeong <ilikerunning@naver.com>
2024-07-31 10:23:00 +0900

    chore: 이미지 div h 고정

--- src/components/member/MemberInviteCard.tsx
+++ src/components/member/MemberInviteCard.tsx
@@ -3,8 +3,11 @@ import { KakaoShareButton } from "../KakaoShareBtn";
 export const MemberInviteCard = () => {
   return (
     <div className="flex flex-col gap-4 border p-6 rounded-lg shadow-md bg-white items-center h-60vh overflow-hidden">
-      <div>
-        <img src="https://qggewtakkrwcclyxtxnz.supabase.co/storage/v1/object/public/prayu/intro_img_2.png" />
+      <div className="h-[300px]">
+        <img
+          className="h-full"
+          src="https://qggewtakkrwcclyxtxnz.supabase.co/storage/v1/object/public/prayu/intro_img_2.png"
+        />
       </div>
 
       <div className="flex flex-col text-center gap-4">
