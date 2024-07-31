9d273a4286c3f9574084bee5380211e9c87332d7
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-07-31 12:08:54 +0900

    feat: Carousel Dots 추가 (#71)
    * feat: Carousel Dots 추가
    
    * chore: 세부 이미지 수정
    
    * chore: 세부 간격 수정
    
    * chore: 이미지 div h 고정
    
    * chore: 그룹 생성 이미지 변경
    
    * chore: 프린트 제거
    
    * chore: 문의하기 버튼 수정
    
    * chore: 기도시작하기 버튼 수정
    
    * chore: tailwind 커스텀 수정

--- src/components/member/MemberInviteCard.tsx
+++ src/components/member/MemberInviteCard.tsx
@@ -2,24 +2,26 @@ import { KakaoShareButton } from "../KakaoShareBtn";
 
 export const MemberInviteCard = () => {
   return (
-    <div className="flex flex-col gap-4 border p-10 rounded-lg shadow-md bg-white items-center h-60vh">
-      <img
-        src="https://qggewtakkrwcclyxtxnz.supabase.co/storage/v1/object/public/prayu/MainPageIntro2.png"
-        className="rounded-md"
-      />
-      <div className="flex flex-col text-center gap-2">
-        <p className="text-lg font-bold">그룹원 초대</p>
-        <div>
-          <p className="text-sm text-gray-500">그룹원을 초대하고</p>
-          <p className="text-sm text-gray-500">오늘의 기도를 시작해 보아요</p>
-        </div>
+    <div className="flex flex-col gap-4 border p-6 rounded-lg shadow-md bg-white items-center h-60vh overflow-hidden">
+      <div className="h-[300px]">
+        <img
+          className="h-full"
+          src="https://qggewtakkrwcclyxtxnz.supabase.co/storage/v1/object/public/prayu/intro_img_2.png"
+        />
       </div>
 
-      <KakaoShareButton
-        groupPageUrl={window.location.href}
-        id="paryTodayIntro"
-        message="카카오톡으로 초대하기"
-      ></KakaoShareButton>
+      <div className="flex flex-col text-center gap-4">
+        <div>
+          <p className="text-sm text-gray-500">
+            그룹원을 초대하고 오늘의 기도를 시작해 보아요
+          </p>
+        </div>
+        <KakaoShareButton
+          groupPageUrl={window.location.href}
+          id="paryTodayIntro"
+          message="카카오톡으로 초대하기"
+        ></KakaoShareButton>
+      </div>
     </div>
   );
 };
