d918bbd8a7fc43f79a9eb1040d84a404c6982065
MMyeong <ilikerunning@naver.com>
2024-08-01 21:00:12 +0900

    chore: 홈화면

--- src/components/GroupMenuBtn.tsx
+++ src/components/GroupMenuBtn.tsx
@@ -68,7 +68,7 @@ const GroupManuBtn: React.FC<GroupManuBtnProps> = ({
             + 그룹 만들기
           </a>
           <hr />
-          <a href="/">홈</a>
+          <a href="/">홈 화면</a>
           <a href={`${import.meta.env.VITE_PRAY_KAKAO_CHANNEL_CHAT_URL}`}>
             문의하기
           </a>
