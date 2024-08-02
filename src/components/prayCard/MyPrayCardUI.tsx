6910469ebb04b3966a7428ee78ff0c67324e83b0
MMyeong <ilikerunning@naver.com>
2024-08-02 17:33:14 +0900

    chore: ref 제거

--- src/components/prayCard/MyPrayCardUI.tsx
+++ src/components/prayCard/MyPrayCardUI.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import useBaseStore from "@/stores/baseStore";
 import { PrayCardWithProfiles } from "supabase/types/tables";
 import { MemberWithProfiles } from "supabase/types/tables";
