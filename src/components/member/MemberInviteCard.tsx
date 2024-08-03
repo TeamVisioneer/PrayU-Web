9979059a43089335e7e8dfdafd1d17954e511f04
MMyeong <ilikerunning@naver.com>
2024-08-03 14:55:49 +0900

    chore: TodayPray 포멧으로 변경

--- src/components/member/MemberInviteCard.tsx
+++ src/components/member/MemberInviteCard.tsx
@@ -1,13 +1,38 @@
-import { KakaoShareButton } from "../share/KakaoShareBtn";
+import { Button } from "../ui/button";
+import useBaseStore from "@/stores/baseStore";
+import { PrayType, PrayTypeDatas } from "@/Enums/prayType";
 
 export const MemberInviteCard = () => {
+  const member = useBaseStore((state) => state.targetMember);
+
+  const reactionImages = [
+    { type: PrayType.PRAY, opacity: "opacity-40" },
+    { type: PrayType.GOOD, opacity: "opacity-70" },
+    { type: PrayType.LIKE, opacity: "" },
+    { profile: true },
+    { type: PrayType.PRAY, opacity: "" },
+    { type: PrayType.GOOD, opacity: "opacity-70" },
+    { type: PrayType.LIKE, opacity: "opacity-40" },
+  ];
   return (
     <div className="flex flex-col flex-grow justify-center items-center text-center gap-5  border rounded-2xl shadow-prayCard bg-gradient-to-b from-start/40 via-middle/40 via-30% to-end/40  ">
-      <div className="h-[180px] flex justify-center">
-        <img
-          className="h-full rounded-md shadow-prayCard"
-          src="/images/KakaoShare.png"
-        />
+      <div className="flex justify-center items-center gap-2">
+        {reactionImages.map((reaction, index) =>
+          reaction.profile ? (
+            <img
+              key={index}
+              src={member?.profiles.avatar_url || ""}
+              className="w-14 h-14 rounded-full ring-2 ring-[#FFBFBD]/50 drop-shadow-[0_0_10px_rgb(255,148,146,0.8)]"
+            />
+          ) : (
+            <img
+              key={index}
+              src={PrayTypeDatas[reaction.type!]?.img}
+              alt={PrayTypeDatas[reaction.type!]?.emoji}
+              className={`w-7 h-7 ${reaction.opacity}`}
+            />
+          )
+        )}
       </div>
       <div className="flex flex-col gap-2">
         <h1 className="font-bold text-xl">그룹원 초대</h1>
@@ -16,12 +41,8 @@ export const MemberInviteCard = () => {
           <h1>기도제목을 공유하고 매일 기도해 주세요</h1>
         </div>
       </div>
-      <KakaoShareButton
-        groupPageUrl={window.location.href}
-        id="paryTodayIntro"
-        message="카카오톡으로 초대하기"
-        eventOption={{ where: "MemberInviteCard" }}
-      ></KakaoShareButton>
+      {/* TODO: 초대 드로워 트리거로 변경 */}
+      <Button variant="primary">그룹원 초대하기</Button>
     </div>
   );
 };
