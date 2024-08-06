6806e6eb22a27928a946eda82d091becf65a8592
MMyeong <ilikerunning@naver.com>
2024-08-06 10:33:26 +0900

    refactor: PrayCardUI PrayCardList 용으로 변경

--- src/components/prayCard/PrayCardUI.tsx
+++ src/components/prayCard/PrayCardUI.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import useBaseStore from "@/stores/baseStore";
 import { PrayCardWithProfiles } from "supabase/types/tables";
-import { MemberWithProfiles } from "supabase/types/tables";
 import { ClipLoader } from "react-spinners";
 import ReactionWithCalendar from "./ReactionWithCalendar";
 import { Textarea } from "../ui/textarea";
@@ -12,14 +11,13 @@ interface EventOption {
 
 interface PrayCardProps {
   currentUserId: string;
-  prayCard: PrayCardWithProfiles | null;
-  member?: MemberWithProfiles | null;
+  prayCard: PrayCardWithProfiles;
   eventOption: EventOption;
 }
 
+// TODO: 현재 TodayPray 에서의 PrayCard 를 위한 컴포넌트로 사용중, 이후 이름 수정 및 폴더링 예정
 const PrayCardUI: React.FC<PrayCardProps> = ({
   currentUserId,
-  member,
   prayCard,
   eventOption,
 }) => {
@@ -28,11 +26,12 @@ const PrayCardUI: React.FC<PrayCardProps> = ({
     fetchPrayDataByUserId: state.fetchPrayDataByUserId,
   }));
 
+  // TODO: 제거 예정. PrayCard.pray 로 사용 예정
   useEffect(() => {
-    if (prayCard?.id) {
+    if (prayCard) {
       fetchPrayDataByUserId(prayCard.id, currentUserId);
     }
-  }, [currentUserId, fetchPrayDataByUserId, prayCard?.id]);
+  }, [currentUserId, fetchPrayDataByUserId, prayCard]);
 
   if (!prayDataHash) {
     return (
@@ -48,36 +47,24 @@ const PrayCardUI: React.FC<PrayCardProps> = ({
         <div className="flex flex-col justify-center items-start gap-1 bg-gradient-to-r from-start/60 via-middle/60 via-30% to-end/60 rounded-t-2xl p-5">
           <div className="flex items-center gap-2">
             <img
-              src={
-                prayCard?.profiles.avatar_url ||
-                member?.profiles.avatar_url ||
-                ""
-              }
+              src={prayCard.profiles.avatar_url || ""}
               className="w-7 h-7 rounded-full"
             />
-            <p className="text-white text-lg">
-              {prayCard?.profiles.full_name || member?.profiles.full_name}
-            </p>
+            <p className="text-white text-lg">{prayCard.profiles.full_name}</p>
           </div>
           <p className="text-sm text-white w-full text-left">
-            시작일 :{" "}
-            {prayCard?.created_at.split("T")[0] ||
-              member?.updated_at.split("T")[0]}
+            시작일 : {prayCard.created_at.split("T")[0]}
           </p>
         </div>
         <div className="flex flex-col flex-grow min-h-full max-h-full items-start px-[10px] py-[10px] overflow-y-auto no-scrollbar">
           <Textarea
             className="flex-grow w-full p-2 rounded-md overflow-y-auto no-scrollbar text-black !opacity-100 !border-none !cursor-default"
-            value={prayCard?.content || member?.pray_summary || ""}
+            value={prayCard.content || ""}
             disabled={true}
           />
         </div>
       </div>
-      <ReactionWithCalendar
-        prayCard={prayCard}
-        eventOption={eventOption}
-        member={member}
-      />
+      <ReactionWithCalendar prayCard={prayCard} eventOption={eventOption} />
     </div>
   );
 };
