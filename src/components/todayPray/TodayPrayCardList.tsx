e27eaccab4d10b2dd1916029cdda3e053c2375f3
MMyeong <ilikerunning@naver.com>
2024-08-07 21:49:53 +0900

    chore: 오늘의 기도 완료 부분 height 조정

--- src/components/todayPray/TodayPrayCardList.tsx
+++ src/components/todayPray/TodayPrayCardList.tsx
@@ -40,7 +40,7 @@ const TodayPrayCardList: React.FC<PrayCardListProps> = ({
   const endDt = getISOTodayDate(1);
 
   const completedItem = (
-    <div className="flex flex-col gap-4 justify-center items-center pb-10">
+    <div className="flex flex-col gap-4 justify-center items-center min-h-[80vh] max-h-[80vh] pb-10">
       <img
         src={PrayTypeDatas["pray"].img}
         alt={PrayTypeDatas["pray"].emoji}
