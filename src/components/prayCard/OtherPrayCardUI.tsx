9cf2a51f5077b4ff4f0b0e23727f29fcd3e1cf8d
MMyeong <ilikerunning@naver.com>
2024-08-08 19:21:47 +0900

    fix: PrayCard 데이터 Member 로 수정

--- src/components/prayCard/OtherPrayCardUI.tsx
+++ src/components/prayCard/OtherPrayCardUI.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import useBaseStore from "@/stores/baseStore";
-import { ClipLoader } from "react-spinners";
 import ReactionWithCalendar from "./ReactionWithCalendar";
 import { Textarea } from "../ui/textarea";
 import { getISOTodayDate } from "@/lib/utils";
@@ -35,19 +34,16 @@ const OtherPrayCardUI: React.FC<OtherPrayCardProps> = ({
 
   if (!otherPrayCardList) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <ClipLoader size={50} color={"#123abc"} loading={true} />
-      </div>
+      <div className="flex justify-center items-center min-h-[80vh] max-h-[80vh]"></div>
     );
   }
 
   // TODO: PrayCardCreateModal 로 반환하기 ( 이미 GroupBody 에서 member.updated_at 으로 처리중 )
-  if (otherPrayCardList.length === 0) {
-    return null;
-  }
+  if (otherPrayCardList.length === 0) return null;
+  if (!otherMember) return null;
 
   const prayCard = otherPrayCardList[0];
-  const isExpiredOtherMember = otherMember!.updated_at < getISOTodayDate(-6);
+  const isExpiredOtherMember = otherMember.updated_at < getISOTodayDate(-6);
 
   const PrayCardUI = () => (
     <div className="flex flex-col gap-6 min-h-[80vh] max-h-[80vh]">
@@ -55,19 +51,21 @@ const OtherPrayCardUI: React.FC<OtherPrayCardProps> = ({
         <div className="flex flex-col justify-center items-start gap-1 bg-gradient-to-r from-start/60 via-middle/60 via-30% to-end/60 rounded-t-2xl p-5">
           <div className="flex items-center gap-2">
             <img
-              src={prayCard.profiles.avatar_url || ""}
+              src={otherMember.profiles.avatar_url || ""}
               className="w-7 h-7 rounded-full"
             />
-            <p className="text-white text-lg">{prayCard.profiles.full_name}</p>
+            <p className="text-white text-lg">
+              {otherMember.profiles.full_name}
+            </p>
           </div>
           <p className="text-sm text-white w-full text-left">
-            시작일 : {prayCard.created_at.split("T")[0]}
+            시작일 : {otherMember.updated_at.split("T")[0]}
           </p>
         </div>
         <div className="flex flex-col flex-grow min-h-full max-h-full items-start px-[10px] py-[10px] overflow-y-auto no-scrollbar">
           <Textarea
             className="flex-grow w-full p-2 rounded-md overflow-y-auto no-scrollbar text-black !opacity-100 !border-none !cursor-default"
-            value={prayCard.content || ""}
+            value={otherMember.pray_summary || ""}
             disabled={true}
           />
         </div>
