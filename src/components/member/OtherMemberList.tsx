6b59569e49b68447561de62e974635dbeb5e626b
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-08-08 10:18:10 +0900

    fix: 오늘의 기도 예외처리 (#105)

--- src/components/member/OtherMemberList.tsx
+++ src/components/member/OtherMemberList.tsx
@@ -4,6 +4,7 @@ import OtherMember from "./OtherMember";
 import MemberInviteCard from "./MemberInviteCard";
 import TodayPrayBtn from "../todayPray/TodayPrayBtn";
 import TodayPrayStartCard from "../todayPray/TodayPrayStartCard";
+import { getISOTodayDate } from "@/lib/utils";
 
 interface OtherMembersProps {
   currentUserId: string;
@@ -23,23 +24,28 @@ const OtherMemberList: React.FC<OtherMembersProps> = ({
   }, [currentUserId, fetchIsPrayToday, groupId]);
 
   if (isPrayToday == null || !memberList) return null;
-  if (memberList.length === 1) return <MemberInviteCard />;
-  if (!isPrayToday) return <TodayPrayStartCard />;
+
+  const otherMemberList = memberList.filter(
+    (member) => member.user_id !== currentUserId
+  );
+  const isExpiredAllMember = otherMemberList.every(
+    (member) => member.updated_at < getISOTodayDate(-6)
+  );
+
+  if (otherMemberList.length === 0) return <MemberInviteCard />;
+  if (!isPrayToday && !isExpiredAllMember) return <TodayPrayStartCard />;
 
   return (
     <div className="flex flex-col gap-2">
       <div className="text-sm text-gray-500 p-2">기도 구성원</div>
       <div className="flex flex-col gap-4">
-        {memberList.map((member) => {
-          if (member.user_id !== currentUserId)
-            return (
-              <OtherMember
-                key={member.id}
-                currentUserId={currentUserId}
-                member={member}
-              ></OtherMember>
-            );
-        })}
+        {otherMemberList.map((member) => (
+          <OtherMember
+            key={member.id}
+            currentUserId={currentUserId}
+            member={member}
+          ></OtherMember>
+        ))}
       </div>
       <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2">
         <TodayPrayBtn eventOption={{ where: "OtherMemberList" }} />
