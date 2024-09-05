b25e0301d035091782f70d1db7c97159a927e6e7
김민수 <minsukim@gimminsuui-MacBookAir.local>
2024-09-05 15:16:06 +0900

    feat: 공지 다이얼로그 백키 반응 추가

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,10 +1,31 @@
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
+import { useEffect } from "react";
 
 import { cn } from "@/lib/utils";
 
-const Dialog = DialogPrimitive.Root;
+const Dialog = ({
+  ...props
+}: React.ComponentProps<typeof DialogPrimitive.Root>) => {
+  // Dialog Custom Start
+  const { onOpenChange } = props;
+
+  useEffect(() => {
+    const handlePopState = () => {
+      if (onOpenChange) {
+        onOpenChange(false);
+      }
+    };
+    window.addEventListener("popstate", handlePopState);
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [onOpenChange]);
+  // Dialog Custom End
+
+  return <DialogPrimitive.Root {...props} />;
+};
 
 const DialogTrigger = DialogPrimitive.Trigger;
 
