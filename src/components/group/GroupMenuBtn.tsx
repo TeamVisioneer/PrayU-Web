c73c4e5b7f7a3fbea23fe24ced0c4506c7e74f56
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-06 11:32:34 +0900

    chore: 내 정보 new

--- src/components/group/GroupMenuBtn.tsx
+++ src/components/group/GroupMenuBtn.tsx
@@ -163,12 +163,18 @@ const GroupManuBtn: React.FC<GroupManuBtnProps> = ({
           )}
 
           <hr />
-          <a
-            href="/myProfile"
-            onClick={() => analyticsTrack("클릭_프로필_나", {})}
-          >
-            내 정보
-          </a>
+          <div className="flex gap-2 items-center">
+            <p className="text-xs flex flex-col justify-center items-center bg-mainBtn rounded-xl px-1 h-fit text-white">
+              new
+            </p>
+            <a
+              href="/myProfile"
+              onClick={() => analyticsTrack("클릭_프로필_나", {})}
+            >
+              내 정보
+            </a>
+          </div>
+
           <a href="/" onClick={() => analyticsTrack("클릭_공유_도메인", {})}>
             PrayU 홈
           </a>
@@ -180,9 +186,6 @@ const GroupManuBtn: React.FC<GroupManuBtnProps> = ({
           </div>
 
           <div className="flex gap-2 items-center">
-            <p className="text-xs flex flex-col justify-center items-center bg-mainBtn rounded-xl px-1 h-fit text-white">
-              new
-            </p>
             <a
               href={`${import.meta.env.VITE_PRAY_KAKAO_CHANNEL_CHAT_URL}`}
               onClick={() => onClickContactUs()}
