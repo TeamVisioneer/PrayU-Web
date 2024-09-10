e5970fc97e6f4324243d01313cda1fc05e6c45de
MMyeong <ilikerunning@naver.com>
2024-09-10 23:03:31 +0900

    chore: profile 이미지 크기 변경

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -8,7 +8,7 @@ export const UserProfile: React.FC<UserProfileProps> = ({ profile }) => {
   return (
     <div className="flex items-center gap-2">
       <img
-        className="w-4 h-4 rounded-full border object-cover"
+        className="w-6 h-6 rounded-full border object-cover"
         src={profile.avatar_url || "/images/defaultProfileImage.png"}
         onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
           e.currentTarget.src = "/images/defaultProfileImage.png";
