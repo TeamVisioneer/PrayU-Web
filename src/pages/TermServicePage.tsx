f12b83c366c18357136c3a029072549f932bbb48
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-11 10:51:59 +0900

    termServicePage 개선:

--- src/pages/TermServicePage.tsx
+++ src/pages/TermServicePage.tsx
@@ -1,11 +1,32 @@
-import { useLocation } from "react-router-dom";
+import useAuth from "@/hooks/useAuth";
+import useBaseStore from "@/stores/baseStore";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const TermServicePage: React.FC = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const getProfile = useBaseStore((state) => state.getProfile);
+  const profile = useBaseStore((state) => state.profile);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const redirectUrl = queryParams.get("redirectUrl");
+  const from = queryParams.get("from");
 
-  console.log("redirectUrl", redirectUrl);
+  useEffect(() => {
+    if (user && !profile) {
+      getProfile(user.id); // 프로필을 가져오는 함수는 한 번만 호출
+    }
+  }, [user, profile, getProfile]);
+
+  useEffect(() => {
+    if (profile && profile.website !== null) {
+      navigate(from!, { replace: true }); // 프로필에 웹사이트가 있으면 리다이렉션
+    }
+  }, [profile, navigate, from]);
+
+  // 프로필이 로드되지 않았을 때는 아무것도 렌더링하지 않음
+  if (!profile) return null;
 
   return <div>TermServicePage</div>;
 };
