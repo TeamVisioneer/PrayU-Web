4ab14e5a86c35716ee22a1477358f59136d6fc0f
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-23 16:50:48 +0900

    feat: ios user agent 대신 params으로 대체

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -23,13 +23,9 @@ const MainPage: React.FC = () => {
 
   const [api, setApi] = useState<CarouselApi>();
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isAppleDevice, setIsAppleDevice] = useState(false);
 
-  useEffect(() => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    const isIOSApp = userAgent.includes("ios_app");
-    setIsAppleDevice(isIOSApp);
-  }, []);
+  const urlParams = new URLSearchParams(window.location.search);
+  const platform = urlParams.get("platform");
 
   useEffect(() => {
     if (!api) return;
@@ -114,7 +110,7 @@ const MainPage: React.FC = () => {
           카카오로 시작하기
         </button>
 
-        {isAppleDevice && (
+        {platform === "ios" && (
           <button
             className="flex items-center gap-3 px-4 py-2 rounded-lg text-sm"
             onClick={handleAppleLoginBtnClick}
