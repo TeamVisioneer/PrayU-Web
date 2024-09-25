d95f6fa3740551634e3df1f093dfd2a33eac7558
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-25 14:40:54 +0900

    design: accoridian 크게 둘러싸기

--- src/components/auth/EmailLoginBtn.tsx
+++ src/components/auth/EmailLoginBtn.tsx
@@ -12,27 +12,30 @@ import { MdEmail } from "react-icons/md";
 
 const EmailLoginBtn = () => {
   return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger
-          onClick={() => analyticsTrack("클릭_이메일_로그인", {})}
-          className="w-full"
-        >
-          <button className="w-full flex justify-between items-center gap-3 px-4 py-2 rounded-lg text-sm bg-gray-200 border-gray-200 border-2">
-            <MdEmail className="w-4 h-4 text-gray-600" />
-            <div className="flex-grow text-gray-600">이메일로 시작하기</div>
-          </button>
-        </AccordionTrigger>
-        <AccordionContent>
-          <Auth
-            supabaseClient={supabase}
-            appearance={{ theme: ThemeSupa }}
-            providers={[]}
-            redirectTo="http://localhost:5173/auth/callback"
-          />
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <div className="bg-gray-200 rounded-lg justify-between ">
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger
+            onClick={() => analyticsTrack("클릭_이메일_로그인", {})}
+            className="w-full"
+          >
+            <button className="w-full flex justify-between items-center gap-3 px-4 py-2 text-sm rounded-lg border-gray-200 border-2">
+              <MdEmail className="w-4 h-4 text-gray-600" />
+              <div className="flex-grow text-gray-600">이메일로 시작하기</div>
+            </button>
+          </AccordionTrigger>
+          <AccordionContent>
+            <div className="p-4">
+              <Auth
+                supabaseClient={supabase}
+                appearance={{ theme: ThemeSupa }}
+                providers={[]}
+              />
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
   );
 };
 
