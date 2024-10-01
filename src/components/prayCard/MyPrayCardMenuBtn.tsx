bd9ec5b2a8be2c419e71dd98a1b11ebf7f0c7ebc
MMyeong <ilikerunning@naver.com>
2024-10-01 17:02:05 +0900

    chore: 워딩 수정

--- src/components/prayCard/MyPrayCardMenuBtn.tsx
+++ src/components/prayCard/MyPrayCardMenuBtn.tsx
@@ -66,7 +66,7 @@ const MyPrayCardMenuBtn: React.FC<MyMoreBtnProps> = ({
     else {
       setAlertData({
         color: "bg-mainBtn",
-        title: "메세지 전송 동의",
+        title: "카카오톡 전송 동의",
         description: `그룹원들과 카카오톡으로 기도요청 메세지를 보내요!`,
         actionText: "계속하기",
         cancelText: "취소",
