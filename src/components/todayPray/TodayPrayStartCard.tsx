51c2b7b2f4bb4270ae5083a87b1760b723fe7a9d
MMyeong <ilikerunning@naver.com>
2024-10-07 21:03:51 +0900

    chore: 오늘의 기도 카드 css 변경

--- src/components/todayPray/TodayPrayStartCard.tsx
+++ src/components/todayPray/TodayPrayStartCard.tsx
@@ -1,43 +1,12 @@
-import { useEffect, useRef, useState } from "react";
 import TodayPrayBtn from "./TodayPrayBtn";
 
 export const TodayPrayStartCard = () => {
-  const divRef = useRef<HTMLDivElement>(null);
-
-  const [size, setSize] = useState({ width: 0, height: 0 });
-
-  useEffect(() => {
-    if (divRef.current) {
-      const handleResize = () => {
-        if (!divRef.current) return;
-        setSize({
-          width: divRef.current.offsetWidth,
-          height: divRef.current.offsetHeight,
-        });
-      };
-      handleResize();
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
-
   return (
-    <div className="w-full h-full flex flex-col items-center">
+    <div className="w-full flex-grow flex flex-col items-center">
       <div className="relative flex flex-col w-[85%] flex-grow justify-center items-center max-h-[500px]">
-        <div
-          className="absolute left-0 bottom-0 transform origin-bottom-left rotate-[-3deg] opacity-50 bg-gradient-to-t from-[#FFDAD7] via-[#FFE5F8] via-41.75% to-[#AAC7FF] rounded-2xl shadow-prayCard z-0"
-          style={{ width: size.width * 0.2, height: size.height * 0.95 }}
-        ></div>
-        <div
-          className="absolute right-0 bottom-0 transform origin-bottom-right rotate-[3deg] opacity-50 bg-gradient-to-t from-[#FFDAD7] via-[#FFE5F8] via-41.75% to-[#AAC7FF] rounded-2xl shadow-prayCard z-0"
-          style={{ width: size.width * 0.2, height: size.height * 0.95 }}
-        ></div>
-        <div
-          className="flex w-full flex-col flex-grow py-10 justify-center items-center text-center gap-6 border rounded-2xl shadow-prayCard bg-gradient-to-t from-[#FFF8F8] via-[#FFEBFA] via-41.75% to-[#AAC7FF] z-10 opacity-100"
-          ref={divRef}
-        >
+        <div className="absolute w-14 h-[95%] left-0 bottom-0 transform origin-bottom-left rotate-[-3deg] opacity-50 bg-gradient-to-t from-[#FFDAD7] via-[#FFE5F8] via-41.75% to-[#AAC7FF] rounded-2xl shadow-prayCard z-0"></div>
+        <div className="absolute w-14 h-[95%]  right-0 bottom-0 transform origin-bottom-right rotate-[3deg] opacity-50 bg-gradient-to-t from-[#FFDAD7] via-[#FFE5F8] via-41.75% to-[#AAC7FF] rounded-2xl shadow-prayCard z-0"></div>
+        <div className="flex w-full flex-col flex-grow py-10 justify-center items-center text-center gap-6 border rounded-2xl shadow-prayCard bg-gradient-to-t from-[#FFF8F8] via-[#FFEBFA] via-41.75% to-[#AAC7FF] z-10 opacity-100">
           <div className="flex flex-col gap-4">
             <h1 className="font-bold text-xl">오늘의 기도</h1>
             <div className="text-grayText">
