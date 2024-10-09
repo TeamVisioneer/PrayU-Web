651545ba2d6d57eb584f10204805d06b0f086507
kim minsu <67274959+yaminsu5674@users.noreply.github.com>
2024-10-09 14:45:02 +0900

    chore: 랜딩페이지 상단 바 추가 (#238)
    Co-authored-by: 김민수 <minsukim@gimminsuui-MacBookAir.local>

--- src/pages/MainPage/MainHeader.tsx
+++ src/pages/MainPage/MainHeader.tsx
@@ -1,6 +1,17 @@
-const FixedHeader: React.FC = () => {
+import { cn } from "@/lib/utils";
+
+interface FixedHeaderProps {
+  className?: string;
+}
+
+const FixedHeader: React.FC<FixedHeaderProps> = ({ className }) => {
   return (
-    <header className="fixed top-0 max-w-[480px] mx-auto w-full bg-mainBg z-50 border-y border-l-gray-500">
+    <header
+      className={cn(
+        "fixed top-0 max-w-[480px] mx-auto w-full bg-mainBg z-50 border-y border-l-gray-500",
+        className
+      )}
+    >
       <div className="flex justify-between items-center py-2 px-4">
         <a href="/" className="flex items-center gap-2">
           <img src="/images/PrayULogo.png" className="w-5 h-5" />
