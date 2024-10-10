347e536abbf23e28250e1ddfffcd5bd41fd81e06
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-10-10 10:11:42 +0900

    feat: 그룹 내보내기 기능 추가 (#240)

--- src/components/group/GroupSettingsDialog.tsx
+++ src/components/group/GroupSettingsDialog.tsx
@@ -7,10 +7,12 @@ import {
 } from "@/components/ui/dialog";
 import useBaseStore from "@/stores/baseStore";
 import { Input } from "../ui/input";
-import { Group } from "supabase/types/tables";
+import { Group, MemberWithProfiles } from "supabase/types/tables";
 import { useEffect } from "react";
 import { Button } from "../ui/button";
 import { analyticsTrack } from "@/analytics/analytics";
+import { UserProfile } from "../profile/UserProfile";
+import { Badge } from "../ui/badge";
 
 interface GroupSettingsDialogProps {
   targetGroup: Group;
@@ -31,6 +33,17 @@ const GroupSettingsDialog: React.FC<GroupSettingsDialogProps> = ({
   const memberList = useBaseStore((state) => state.memberList);
   const getGroup = useBaseStore((state) => state.getGroup);
 
+  const setAlertData = useBaseStore((state) => state.setAlertData);
+  const setIsConfirmAlertOpen = useBaseStore(
+    (state) => state.setIsConfirmAlertOpen
+  );
+  const deleteMemberbyGroupId = useBaseStore(
+    (state) => state.deleteMemberbyGroupId
+  );
+  const deletePrayCardByGroupId = useBaseStore(
+    (state) => state.deletePrayCardByGroupId
+  );
+
   const onClickSaveGroup = async () => {
     if (inputGroupName.trim() === "") return;
     analyticsTrack("클릭_그룹_이름변경", { group_name: GroupSettingsDialog });
@@ -39,6 +52,23 @@ const GroupSettingsDialog: React.FC<GroupSettingsDialogProps> = ({
       getGroup(targetGroup.id);
       setIsOpenGroupSettingsDialog(false);
     }
+  };
+
+  const onClickDeleteMemberInGroup = (member: MemberWithProfiles) => {
+    setAlertData({
+      color: "bg-red-400",
+      title: "그룹 내보내기",
+      description: `해당 그룹에서  ${member.profiles.full_name} 님을 내보내시겠어요?\n*${member.profiles.full_name} 님의 기도카드는 모두 삭제되어요`,
+      actionText: "내보내기",
+      cancelText: "취소",
+      onAction: async () => {
+        await deleteMemberbyGroupId(member.profiles.id, targetGroup.id);
+        await deletePrayCardByGroupId(member.profiles.id, targetGroup.id);
+        window.location.replace(`/group/${targetGroup.id}`);
+        analyticsTrack("클릭_그룹_내보내기", { group_id: targetGroup.id });
+      },
+    });
+    setIsConfirmAlertOpen(true);
   };
 
   useEffect(() => {
@@ -58,7 +88,7 @@ const GroupSettingsDialog: React.FC<GroupSettingsDialogProps> = ({
           <DialogDescription></DialogDescription>
         </DialogHeader>
         <div className="flex flex-col gap-8">
-          <div className="flex flex-col gap-2">
+          <section className="flex flex-col gap-2">
             <label className="text-sm font-medium text-gray-700">그룹명</label>
             <Input
               type="text"
@@ -66,8 +96,38 @@ const GroupSettingsDialog: React.FC<GroupSettingsDialogProps> = ({
               onChange={(e) => setGroupName(e.target.value)}
               maxLength={12}
             />
-          </div>
-          <div className="flex gap-2 justify-center">
+          </section>
+          <section className="flex flex-col gap-4">
+            <label className="text-sm font-medium text-gray-700">
+              그룹원 ({memberList.length})
+            </label>
+            <div className="flex flex-col gap-3 max-h-40 p-1 overflow-auto">
+              {[...memberList]
+                .sort((member) =>
+                  member.user_id == targetGroup.user_id ? -1 : 1
+                )
+                .map((member) => (
+                  <div key={member.id} className="flex justify-between">
+                    <UserProfile
+                      profile={member.profiles}
+                      imgSize="w-7 h-7"
+                      fontSize="font-midium"
+                    />
+                    {member.user_id === targetGroup.user_id ? (
+                      <Badge>그룹장</Badge>
+                    ) : (
+                      <Badge
+                        variant="secondary"
+                        onClick={() => onClickDeleteMemberInGroup(member)}
+                      >
+                        내보내기
+                      </Badge>
+                    )}
+                  </div>
+                ))}
+            </div>
+          </section>
+          <footer className="flex gap-2 justify-center">
             <Button
               variant="secondary"
               className="w-20"
@@ -80,9 +140,9 @@ const GroupSettingsDialog: React.FC<GroupSettingsDialogProps> = ({
               className="w-20"
               onClick={() => onClickSaveGroup()}
             >
-              저장
+              확인
             </Button>
-          </div>
+          </footer>
         </div>
       </DialogContent>
     </Dialog>
