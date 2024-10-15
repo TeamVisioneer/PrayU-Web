fe4450cef6cd8a29de38cd22225efddbf36b710d
MMyeong <ilikerunning@naver.com>
2024-10-16 01:46:45 +0900

    chore: delete API 수정

--- supabase/functions/delete-user/index.ts
+++ supabase/functions/delete-user/index.ts
@@ -1,27 +1,8 @@
 import { corsHeaders } from "../_shared/cors.ts";
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { createClient } from "jsr:@supabase/supabase-js@2";
+import { supabase } from "../serverClient.ts";
 
-serve(async (req) => {
-  if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
-  }
+Deno.serve(async (req) => {
   const { userId } = await req.json();
-  console.log("userId", userId);
-  const supabaseAdminKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
-  const supabaseUrl = Deno.env.get("SUPABASE_URL");
-
-  console.log("supabaseAdminKey", supabaseAdminKey, "supabaseUrl", supabaseUrl);
-
-  if (!supabaseAdminKey || !supabaseUrl) {
-    return new Response(JSON.stringify({ error: "Missing configuration." }), {
-      status: 500,
-      headers: { ...corsHeaders, "Content-Type": "application/json" },
-    });
-  }
-
-  const supabase = createClient(supabaseUrl, supabaseAdminKey);
-
   const { data, error } = await supabase.auth.admin.deleteUser(userId);
 
   if (error) {
