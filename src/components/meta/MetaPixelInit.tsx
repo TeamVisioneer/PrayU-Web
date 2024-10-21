76abdc0b6c364d06f4b9b7ce178c05aba289d818
김민수 <minsukim@gimminsuui-MacBookAir.local>
2024-10-21 17:22:59 +0900

    fix: 픽셀태그 삽입 위치 변경

---
+++ src/components/meta/MetaPixelInit.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const MetaPixelInit: React.FC = () => {
+  return (
+    <Helmet>
+      <script>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '3851311148517680');
+          fbq('track', 'PageView');
+        `}
+      </script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=3851311148517680&ev=PageView&noscript=1"
+        />
+      </noscript>
+    </Helmet>
+  );
+};
+
+export default MetaPixelInit;
