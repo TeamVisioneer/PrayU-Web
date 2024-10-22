aef4c2b34784778822ada5732c1da50e55afdd1a
MMyeong <ilikerunning@naver.com>
2024-10-22 19:05:35 +0900

    feat: 어드민 페이지 추가

---
+++ src/pages/AdminPage/AdminPage.tsx
@@ -0,0 +1,78 @@
+import useAuth from "@/hooks/useAuth";
+import MainHeader from "../MainPage/MainHeader";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import NotificationSendDialog from "./NotificationDialog";
+import useBaseStore from "@/stores/baseStore";
+import { useEffect } from "react";
+import { getISOTodayDate } from "@/lib/utils";
+
+const AdminPage = () => {
+  const { user } = useAuth();
+  const profileCount = useBaseStore((state) => state.profileCount);
+  const fetchProfileCount = useBaseStore((state) => state.fetchProfileCount);
+  const fetchGroupListByDate = useBaseStore(
+    (state) => state.fetchGroupListByDate
+  );
+  const todayGroupList = useBaseStore((state) => state.todayGroupList);
+
+  const todayDate = getISOTodayDate();
+  useEffect(() => {
+    fetchProfileCount();
+    fetchGroupListByDate(todayDate);
+  }, [fetchProfileCount, fetchGroupListByDate, todayDate]);
+
+  if (!user || user.user_metadata.email !== "team.visioneer15@gmail.com") {
+    window.location.replace("/");
+  }
+
+  return (
+    <div className="flex flex-grow flex-col gap-10 w-full items-center pt-11">
+      <MainHeader />
+      <section className="flex flex-col gap-4 w-full items-start">
+        <h1 className="">대시보드</h1>
+        <div className="flex gap-2 w-full">
+          <Card className="w-1/3 overflow-hidden transition-all duration-200 hover:shadow-lg">
+            <CardHeader className="flex flex-row pb-2 space-y-0">
+              <CardTitle className="w-full text-sm font-medium text-center">
+                누적 유저수
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold text-center">
+                {profileCount}
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="w-1/3 overflow-hidden transition-all duration-200 hover:shadow-lg">
+            <CardHeader className="flex flex-row justify-between pb-2 space-y-0">
+              <CardTitle className="w-full text-sm font-medium text-center">
+                신규 유저수
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold text-center">00</div>
+            </CardContent>
+          </Card>
+          <Card className="w-1/3 overflow-hidden transition-all duration-200 hover:shadow-lg">
+            <CardHeader className="flex flex-row justify-between pb-2 space-y-0">
+              <CardTitle className="w-full text-sm font-medium text-center">
+                신규 그룹수
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold text-center">
+                {todayGroupList?.length || 0}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </section>
+      <section className="flex flex-col gap-4 w-full items-start">
+        <h1 className="">Admin 추가기능</h1>
+        <NotificationSendDialog />
+      </section>
+    </div>
+  );
+};
+
+export default AdminPage;
---
+++ src/pages/AdminPage/NotificationDialog.tsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { AlertCircle, Bell } from "lucide-react";
+import useBaseStore from "@/stores/baseStore";
+import { NotificationType } from "@/components/notification/NotificationType";
+
+const NotificationSendDialog = () => {
+  const [startId, setStartId] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [sendResult, setSendResult] = useState("전송 전");
+
+  const fetchProfileListByStartId = useBaseStore(
+    (state) => state.fetchProfileListByStartId
+  );
+  const createNotification = useBaseStore((state) => state.createNotification);
+
+  const handleSubmit = async () => {
+    setSendResult("전송중");
+    const limit = 1000;
+    let isSending = true;
+
+    while (isSending) {
+      const userIds = await fetchProfileListByStartId(startId, limit);
+      if (!userIds) {
+        isSending = false;
+        setSendResult(`[전송 실패] startId : ${startId}`);
+        break;
+      }
+      const notification = await createNotification({
+        userId: userIds,
+        title: title,
+        body: content,
+        type: NotificationType.NOTICE,
+      });
+      if (!notification) {
+        isSending = false;
+        setSendResult(`[전송 실패] startId : ${startId}`);
+        break;
+      }
+      if (userIds.length < limit) {
+        isSending = false;
+        setSendResult(`전송 완료`);
+        break;
+      } else setStartId(userIds[limit - 1]);
+    }
+
+    setSendResult("전송 완료");
+  };
+
+  return (
+    <div className="flex items-center justify-center p-4">
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <Button className="flex items-center gap-2">
+            <Bell className="h-4 w-4" />
+            공지사항 알림 작성
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="w-11/12 rounded-2xl">
+          <DialogHeader>
+            <DialogTitle className="text-center">
+              공지사항 알림 작성
+            </DialogTitle>
+          </DialogHeader>
+          <section className="space-y-4 mt-4">
+            <div className="space-y-2">
+              <label htmlFor="title" className="text-sm font-medium">
+                제목
+              </label>
+              <Input
+                id="title"
+                placeholder="알림 제목을 입력하세요"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="content" className="text-sm font-medium">
+                내용
+              </label>
+              <Textarea
+                id="content"
+                placeholder="알림 내용을 입력하세요"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                required
+                className="min-h-[100px]"
+              />
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="startId" className="text-sm font-medium">
+                startId
+              </label>
+              <Input
+                id="startId"
+                value={startId}
+                onChange={(e) => setStartId(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="content" className="text-sm font-medium">
+                전송 결과
+              </label>
+              <Input value={sendResult} disabled={true} />
+            </div>
+            <Button
+              onClick={() => handleSubmit()}
+              // disabled={sendResult == "전송 전" ? false : true}
+              className="w-full"
+            >
+              <AlertCircle className="mr-2 h-4 w-4" />
+              알림 전송
+            </Button>
+          </section>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default NotificationSendDialog;
