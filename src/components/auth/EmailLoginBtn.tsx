85c24e08a93d84a7f3178a1e286de358dc9e6f5e
Junbeom Park <136706252+treephesians@users.noreply.github.com>
2024-10-30 16:16:18 +0900

    refactor: 다른 방법으로 로그인 (#298)

--- src/components/auth/EmailLoginBtn.tsx
+++ src/components/auth/EmailLoginBtn.tsx
@@ -12,11 +12,10 @@ const EmailLoginBtn: React.FC = () => {
 
   return (
     <button
-      className="w-full flex justify-between items-center gap-3 px-4 py-2 rounded-lg text-sm border-gray-200 border-2 bg-gray-200"
+      className="flex justify-center items-center w-9 h-9 bg-gray-200 rounded-full"
       onClick={() => handleEmailLoginBtnClick()}
     >
       <MdEmail className="w-[18px] h-[18px] text-gray-500" />
-      <div className="flex-grow text-gray-500">이메일로 시작하기</div>
     </button>
   );
 };
--- src/components/auth/LogInDrawer.tsx
+++ src/components/auth/LogInDrawer.tsx
@@ -48,7 +48,7 @@ const LogInDrawer = () => {
 
   const LoginContent = (
     <div className="flex flex-col gap-6 px-10">
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-1">
         <div className="text-xl font-semibold">로그인 / 회원가입</div>
         <p className="flex flex-col justify-center font-light text-[0.95rem]">
           여러분의 신앙생활은 소중한 개인의 영역입니다.
@@ -56,31 +56,26 @@ const LogInDrawer = () => {
           어떤 정보도 외부에 공유되지 않으니 안심하세요.
         </p>
       </div>
-      <div className="flex flex-col w-full justify-center gap-2">
+      <div className="flex flex-col w-full justify-center gap-4 pb-3">
         <KakaoLoginBtn
           redirectUri={`${baseUrl}/auth/kakao/callback`}
           state={`groupId:${groupId}`}
         />
         {isApp && (
-          <>
-            {isIOS && <AppleLoginBtn redirectUrl={redirectUrl} />}
-            <EmailLoginBtn />
-          </>
+          <div className="flex flex-col items-center gap-3">
+            <div className="flex items-center w-full">
+              <div className="border-t border-gray-300 flex-grow"></div>
+              <span className="text-xs font-light px-4">
+                다른 방법으로 로그인
+              </span>
+              <div className="border-t border-gray-300 flex-grow"></div>
+            </div>
+            <div className="flex justify-center gap-5">
+              {isIOS && <AppleLoginBtn redirectUrl={redirectUrl} />}
+              <EmailLoginBtn />
+            </div>
+          </div>
         )}
-      </div>
-      <div className="flex flex-col w-full justify-center gap-1 text-sm text-gray-400">
-        <hr className="border-gray-300 mb-1" />
-        <div className="flex gap-2">
-          <p className="font-light">
-            로그인 / 회원 관련 궁금하신 사항이 있다면?
-          </p>
-          <a
-            href={`${import.meta.env.VITE_PRAY_KAKAO_CHANNEL_CHAT_URL}`}
-            className="text-blue-500 cursor-pointer"
-          >
-            문의하기
-          </a>
-        </div>
       </div>
     </div>
   );
