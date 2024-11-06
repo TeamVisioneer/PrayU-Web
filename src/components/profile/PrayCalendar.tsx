829700ec3c2d7bdc2235ccbed4806f45cfa731ee
김민수 <minsukim@gimminsuui-MacBookAir.local>
2024-11-06 17:00:51 +0900

    chore: 연월 제거

--- src/components/profile/PrayCalendar.tsx
+++ src/components/profile/PrayCalendar.tsx
@@ -38,7 +38,6 @@ const PrayCalendar = () => {
     <div className="aspect-[2.5] w-full flex-grow flex flex-col gap-1 bg-white p-5 rounded-xl">
       <div className="flex flex-row justify-between">
         <span className="items-start text-sm font-semibold">기도 캘린더</span>
-        <span className="text-right text-xs font-semibold">2024년 10월</span>
       </div>
       <div className="border-t border-[#f7f7f7] my-2"></div>
       <div className="w-full flex justify-around ">
