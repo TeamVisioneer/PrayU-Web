954d2f4e690f6ab9ceb488b5be79b7841247113d
Junbeom Park <ephesi4ns623@gmail.com>
2024-11-07 14:14:20 +0900

    feat: add analytics, animation + refactor code

--- src/apis/openai.ts
+++ src/apis/openai.ts
@@ -7,12 +7,25 @@ export interface BibleVerseData {
   nature: string;
 }
 
+export interface QTData {
+  scripture: { text: string; reference: string };
+  meditation: {
+    introduction: string;
+    key_messages: { title: string; points: string[] }[];
+  };
+  application_questions: { question: string }[];
+  practical_tasks: { task: string }[];
+  error: string;
+}
+
 export const createBibleVerse = async (
-  content: string,
+  content: string
 ): Promise<BibleVerseData[]> => {
   try {
     const response = await fetch(
-      `${import.meta.env.VITE_SUPA_PROJECT_URL}/functions/v1/openai/bible-verse`,
+      `${
+        import.meta.env.VITE_SUPA_PROJECT_URL
+      }/functions/v1/openai/bible-verse`,
       {
         method: "POST",
         headers: {
@@ -20,7 +33,7 @@ export const createBibleVerse = async (
           authorization: `Bearer ${import.meta.env.VITE_SUPA_ANON_KEY}`,
         },
         body: JSON.stringify({ content }),
-      },
+      }
     );
 
     const { data } = await response.json();
@@ -34,7 +47,9 @@ export const createBibleVerse = async (
 export const fetchBgImage = async (content: string): Promise<string[]> => {
   try {
     const response = await fetch(
-      `${import.meta.env.VITE_SUPA_PROJECT_URL}/functions/v1/openai/bible-image`,
+      `${
+        import.meta.env.VITE_SUPA_PROJECT_URL
+      }/functions/v1/openai/bible-image`,
       {
         method: "POST",
         headers: {
@@ -42,7 +57,7 @@ export const fetchBgImage = async (content: string): Promise<string[]> => {
           authorization: `Bearer ${import.meta.env.VITE_SUPA_ANON_KEY}`,
         },
         body: JSON.stringify({ content }),
-      },
+      }
     );
 
     const { data } = await response.json();
@@ -51,4 +66,20 @@ export const fetchBgImage = async (content: string): Promise<string[]> => {
     Sentry.captureException(error);
     return [];
   }
+};
+
+export const createQT = async (content: string): Promise<QTData> => {
+  const response = await fetch(
+    `http://127.0.0.1:54321/functions/v1/openai/qt`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${import.meta.env.VITE_SUPA_ANON_KEY}`,
+      },
+      body: JSON.stringify({ content }),
+    }
+  );
+  if (!response.ok) throw new Error("Failed to fetch QT data.");
+  return response.json();
 };
