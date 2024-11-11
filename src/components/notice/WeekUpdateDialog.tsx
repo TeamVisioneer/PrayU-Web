bacba6446a86d482107e1ab9594f8780b40b1f51
김민수 <minsukim@gimminsuui-MacBookAir.local>
2024-11-11 15:07:09 +0900

    chore: 이미지 크기 조정&& 멘트 수정

--- src/components/notice/WeekUpdateDialog.tsx
+++ src/components/notice/WeekUpdateDialog.tsx
@@ -21,40 +21,39 @@ const WeekUpdateDialog = () => {
       open={isOpenWeekUpdateDialog}
       onOpenChange={setIsOpenWeekUpdateDialog}
     >
-      <DialogContent className="w-11/12 h-96 overflow-auto rounded-2xl bg-mainBg">
+      <DialogContent className="w-11/12 h-auto overflow-auto rounded-2xl bg-mainBg">
         <DialogHeader>
           <DialogTitle className="text-xl text-left">
-            기도카드 주기 변경 안내
+            기도제목 주기 변경 안내
           </DialogTitle>
           <DialogDescription></DialogDescription>
           <div className="w-full flex flex-col gap-1 py-3 items-left">
-            {/* <span className="text-xs text-left pl-3">이전</span> */}
             <div className="flex items-start pl-2">
               <Badge className="text-xs">이전</Badge>
             </div>
-            <div className="border border-gray-300 rounded-md block p-2">
+            <div className="flex flex-col items-center border border-gray-300 rounded-md p-2">
               <img
                 src="/images/beforeWeek.png"
                 alt="beforeWeek"
-                className="rounded-md"
+                className="h-14 rounded-md"
               />
             </div>
 
             <div className="flex items-start pl-2">
               <Badge className="text-xs">이후</Badge>
             </div>
-            <div className="border border-gray-300 rounded-md block p-2">
+            <div className="flex flex-col items-center border border-gray-300 rounded-md p-2">
               <img
                 src="/images/afterWeek.png"
                 alt="afterWeek"
-                className="rounded-md"
+                className="h-14 rounded-md"
               />
             </div>
 
             <div className="flex flex-col items-start mt-3">
               <p className="text-sm text-left text-gray-400 pt-1">
-                기존에는 작성일로부터 일주일 동안 노출됐는데, 이제는 작성 날짜에
-                상관없이 해당 주차까지만 보여드려요:)
+                기존에는 기도제목을 어느 요일에도 작성할 수 있었는데, 이제부턴
+                일요일에만 기도제목을 작성할 수 있어요:)
               </p>
             </div>
           </div>
