4fd13bc84c64eed96cb75de1a372595c70ac9b3e
MMyeong <ilikerunning@naver.com>
2024-12-21 04:09:22 +0900

    update: d앱설치 버튼 추가

--- src/pages/MainPage/MainHeader.tsx
+++ src/pages/MainPage/MainHeader.tsx
@@ -1,10 +1,21 @@
+import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 
-interface FixedHeaderProps {
+interface MainHeaderProps {
   className?: string;
 }
 
-const FixedHeader: React.FC<FixedHeaderProps> = ({ className }) => {
+const MainHeader: React.FC<MainHeaderProps> = ({ className }) => {
+  const onClickAppInstall = () => {
+    if (navigator.userAgent.match(/Android/i)) {
+      window.location.href =
+        "https://play.google.com/store/apps/details?id=com.team.visioneer.prayu";
+    } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+      window.location.href =
+        "https://itunes.apple.com/kr/app/apple-store/id6711345171";
+    }
+  };
+
   return (
     <header
       className={cn(
@@ -14,12 +25,21 @@ const FixedHeader: React.FC<FixedHeaderProps> = ({ className }) => {
     >
       <div className="flex justify-between items-center py-2 px-4">
         <a href="/" className="flex items-center gap-2">
-          <img src="/images/PrayULogo.png" className="w-5 h-5" />
+          <img src="/images/PrayULogoV3.png" className="w-5 h-5" />
           <h1 className="text-lg font-bold">PrayU</h1>
         </a>
+        {!navigator.userAgent.match(/prayu/i) && (
+          <Badge
+            variant="secondary"
+            className="text-base font-normal border-gray-300 rounded-sm"
+            onClick={() => onClickAppInstall()}
+          >
+            앱설치
+          </Badge>
+        )}
       </div>
     </header>
   );
 };
 
-export default FixedHeader;
+export default MainHeader;
