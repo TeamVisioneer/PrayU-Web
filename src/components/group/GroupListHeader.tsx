b67ec65865e483df5600869d8e11caa2f346eca8
MMyeong <ilikerunning@naver.com>
2025-03-26 21:00:23 +0900

    feat: PrayU Office 경로 임시 지정

--- src/components/group/GroupListHeader.tsx
+++ src/components/group/GroupListHeader.tsx
@@ -1,10 +1,19 @@
 import GroupMenuBtn from "./GroupMenuBtn";
 import NotificationBtn from "../notification/NotificationBtn";
+import { useNavigate } from "react-router-dom";
 
 const GroupListHeader: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <header className="sticky top-0 flex items-center justify-between p-5 z-10 border-b border-gray-200 bg-mainBg">
-      <h1 className="text-xl font-bold text-[#222222]">그룹 홈</h1>
+      <h1
+        onClick={() => {
+          navigate("/office/union");
+        }}
+        className="text-xl font-bold text-[#222222]"
+      >
+        그룹 홈
+      </h1>
       <div className="flex items-center gap-2">
         <NotificationBtn />
         <GroupMenuBtn />
